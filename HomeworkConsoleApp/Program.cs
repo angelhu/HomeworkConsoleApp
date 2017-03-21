7f5365a54fafe7356b6d3df9c26c2236e494f6b9
Angel Hu <angelqhu513@gmail.com>
2017-03-21 15:18:03 -0500

    finished homework

--- HomeworkConsoleApp/Program.cs
+++ HomeworkConsoleApp/Program.cs
@@ -10,7 +10,20 @@ namespace HomeworkConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string helloWorld = "Hello World";
+            int year = 1492;
+            bool dogAtHome = true;
+            DateTime date = DateTime.Today;
+            double money = 5.45;
+            char x = 'X';
+
+            Console.WriteLine(helloWorld);
+            Console.WriteLine("Columbus Sailed the world in " + year + ".");
+            Console.WriteLine("I spent $" + money + " on a cheeseburger.");
+            Console.WriteLine("It is " + dogAtHome + " that I have a dog at home.");
+            Console.WriteLine("Today is " + date.ToString("MM/dd/yyyy") + ".");
+            Console.WriteLine(x + " marks the spot");
+
         }
     }
 }
