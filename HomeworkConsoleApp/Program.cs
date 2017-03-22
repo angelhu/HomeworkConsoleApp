aebb844e8ce71a51d7e5abdff9f59006c58745c0
Angel Hu <angelqhu513@gmail.com>
2017-03-22 15:59:58 -0500

    finished epic mode

--- HomeworkConsoleApp/Program.cs
+++ HomeworkConsoleApp/Program.cs
@@ -13,7 +13,7 @@ namespace HomeworkConsoleApp
             string helloWorld = "Hello World";
             int year = 1492;
             bool dogAtHome = true;
-            DateTime date = DateTime.Today;
+            DateTime date = DateTime.Now;
             double money = 5.45;
             char x = 'X';
 
@@ -23,6 +23,32 @@ namespace HomeworkConsoleApp
             Console.WriteLine("It is " + dogAtHome + " that I have a dog at home.");
             Console.WriteLine("Today is " + date.ToString("MM/dd/yyyy") + ".");
             Console.WriteLine(x + " marks the spot");
+
+
+            /* Ask the User for number a random number.
+               Save that number to a int
+               Use the int.TryParse() Method to parse out the number
+               If the user didnt input a number, repeat step 1 - 4 until they put in a number */
+            bool stopRunning = true;
+
+            while (stopRunning) { 
+            {
+                Console.Write("Please enter a random number: ");
+                string randomNumber = Console.ReadLine();
+                int randomNumberEntered = 0;
+
+
+                if (int.TryParse(randomNumber, out randomNumberEntered))
+                {
+                    Console.WriteLine($"You just entered {randomNumberEntered}!");
+                    stopRunning = false;
+                }
+                else
+                {
+                    Console.WriteLine("Uh oh, that's not a valid number.");
+                }
+            }
+        }
 
         }
     }
